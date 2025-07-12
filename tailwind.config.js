3f55241fda82a9662b4ae638f736f79632f66cde
AlterFM <140619531+AlterFM@users.noreply.github.com>
2025-07-12 14:42:54 +0700

    Convert Login&Register to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.html", // Jika ada index.html di root
-    "./pages/**/*.{html,js,ts,jsx,tsx}", // Memindai semua .html DAN .js di dalam folder 'pages'
+    "./*.html", 
+    "./pages/**/*.{html,js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
